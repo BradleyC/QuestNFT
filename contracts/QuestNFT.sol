765238cf9b415f72745dbe4242cedfe7136e07b1
John Palmer <johnpalmer@Johns-MacBook-Pro.local>
2022-06-25 14:27:57 -0400

    add mint + modifiers

--- contracts/QuestNFT.sol
+++ contracts/QuestNFT.sol
@@ -6,9 +6,37 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 
 contract QuestNFT is ERC721, Ownable, Pausable {
+    uint256 publicMintPrice;
+    uint256 internal _nextId;
+
+    // ============ MODIFIERS ============
+
+    modifier publicMintPaid() {
+        require(msg.value == publicMintPrice, 'QuestNFT: invalid mint fee');
+        _;
+    }
+
+    modifier onlyTokenOwner(uint256 tokenId) {
+        require(msg.sender == ERC721.ownerOf(tokenId), 'Shields: only owner can build Shield');
+        _;
+    }
+
+    // ============ OWNER INTERFACE ============
+
+    function collectFees() external onlyOwner {
+        (bool success, ) = payable(msg.sender).call{value: address(this).balance}(new bytes(0));
+        require(success, 'QuestNFT: ether transfer failed');
+    }
+
+    function setPublicMintPrice(uint256 _publicMintPrice) external onlyOwner {
+      publicMintPrice = _publicMintPrice;
+    }
+
     // Quest Registry
 
-    // Mint Quest NFT
+    function mint(address to) public payable publicMintPaid {
+        _mint(to, _nextId++);
+    }
 
     // Evaluator
 
@@ -17,6 +45,6 @@ contract QuestNFT is ERC721, Ownable, Pausable {
     // Quest NFT Utility Functions
 
     // Quest NFT Modifiers
-
+    
 
 }
