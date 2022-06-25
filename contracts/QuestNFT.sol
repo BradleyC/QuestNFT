fc5657a4c2d51b5e3ef088ef0473653f14a7ba67
Bradley C <bradleyctclarke@gmail.com>
2022-06-25 14:08:20 -0400

    adding contract basics and hardhat config

---
+++ contracts/QuestNFT.sol
@@ -0,0 +1,22 @@
+// SPDX MITNFA
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+
+contract QuestNFT is ERC721, Ownable, Pausable {
+    // Quest Registry
+
+    // Mint Quest NFT
+
+    // Evaluator
+
+    // Base Quest Functions
+
+    // Quest NFT Utility Functions
+
+    // Quest NFT Modifiers
+
+
+}
